0df9a5e9f8d3d919f4c6c220abc08a4cce128ce7
Alex Obogbare <Onozorgheneho1@>
2026-02-04 15:31:48 +0400

    Auto-generated commit

--- chunk-M7AQJ5WQ.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-GAL4ENT6.js";var t=[{path:"",loadComponent:()=>import("./chunk-5W5XUDEZ.js").then(o=>o.Tables)}];export{t as TABLES_ROUTES};
---
+++ chunk-NESPCFVF.js
@@ -0,0 +1,1 @@
+import"./chunk-GAL4ENT6.js";var t=[{path:"",loadComponent:()=>import("./chunk-X3RBOVXM.js").then(o=>o.Checkout)}];export{t as CHECKOUT_ROUTES};
